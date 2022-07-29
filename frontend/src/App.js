6d5cfb858d4834a2205a6406d664a69765fc54d7
Abhinav Aggarwal <abhinavaggarwal23@yahoo.com>
2022-07-29 23:49:28 +0530

    react js setup with frontend framework and initial express js setup

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default App
