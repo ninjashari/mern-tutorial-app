765aa432eed8f6c062696e8a6f1990fc36a7395b
Abhinav Aggarwal <abhinavaggarwal23@yahoo.com>
2022-08-25 23:15:17 +0530

    cart reducer and add to cart action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,13 +5,23 @@ import {
   productListReducer,
   productDetailsReducer,
 } from './reducers/productReducer'
+import { cartReducer } from './reducers/cartReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 })
 
-const initialState = {}
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : []
+
+const initialState = {
+  cart: {
+    cartItems: { cartItems: cartItemsFromStorage },
+  },
+}
 
 const middleware = [thunk]
 
